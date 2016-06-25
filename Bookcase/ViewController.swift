cf24a1c8559fddf085410cd1975ff6162fbe072f
Craig Grummitt <craiggrummitt@gmail.com>
2016-06-25 13:49:31 -0500

    Migrated auto layout to use stack views

--- Bookcase/ViewController.swift
+++ Bookcase/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var book: UIImageView!
+    @IBOutlet weak var isbnStackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {return}
+        if touch.view == book {
+            isbnStackView.isHidden = !isbnStackView.isHidden
+        }
+    }
 
 }
 
