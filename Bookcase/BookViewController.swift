7309f475bba8a823a5d551ce2650f753983a08c2
Craig Grummitt <craiggrummitt@gmail.com>
2016-07-25 16:54:46 -0500

    Resolve layoutRename Book BookCoverRename ViewController BookViewControllerSwitch tapping on book with tapping on infoButton

--- Bookcase/BookViewController.swift
+++ Bookcase/BookViewController.swift
@@ -8,23 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BookViewController: UIViewController {
 
-    @IBOutlet weak var book: UIImageView!
+    @IBOutlet weak var bookCover: UIImageView!
     @IBOutlet weak var isbnStackView: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let infoButton = UIButton(type: .infoLight)
+        infoButton.addTarget(self, action: #selector(toggleISBN), for: .touchUpInside)
+        bookCover.addSubview(infoButton)
+    }
+    func toggleISBN() {
+        self.isbnStackView.isHidden = !self.isbnStackView.isHidden
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard let touch = touches.first else {return}
-        if touch.view == book {
-            isbnStackView.isHidden = !isbnStackView.isHidden
-        }
     }
 
 }
