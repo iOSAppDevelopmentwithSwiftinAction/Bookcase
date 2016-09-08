dde400c87bf8cc9453c9fef01600fec15e32d90d
Craig Grummitt <craiggrummitt@gmail.com>
2016-09-07 22:49:48 -0500

    Small fix

--- Bookcase/BooksCollectionViewController.swift
+++ Bookcase/BooksCollectionViewController.swift
@@ -32,10 +32,6 @@ class BooksCollectionViewController: UICollectionViewController,Injectable {
         }
         collectionView?.reloadData()
     }
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        NotificationCenter.default.removeObserver(self)
-    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
