5d6664233353322558b2c6d51b188976955948a5
Craig Grummitt <craiggrummitt@gmail.com>
2016-09-07 21:43:13 -0500

    Update

--- Bookcase/AppDelegate.swift
+++ Bookcase/AppDelegate.swift
@@ -40,12 +40,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-    func application(_ application: UIApplication,
-                     shouldSaveApplicationState coder: NSCoder) -> Bool {
+    func application(_ application: UIApplication, shouldSaveApplicationState coder: NSCoder) -> Bool {
         return true
     }
-    func application(_ application: UIApplication,
-                     shouldRestoreApplicationState coder: NSCoder) -> Bool {
+    func application(_ application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool {
         return true
     }
 
