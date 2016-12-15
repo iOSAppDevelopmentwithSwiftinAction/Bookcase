92825468036278737fa8cf5d9b7234ba66e5566d
Craig Grummitt <craiggrummitt@gmail.com>
2016-12-15 12:31:11 +1100

    New branch

--- Bookcase/BooksManager.swift
+++ Bookcase/BooksManager.swift
@@ -88,26 +88,7 @@ class BooksManager {
         storeBooks()
     }
     func sampleBooks()->[Book] {
-        var books = [
-            Book(title: "Great Expectations", author: "Charles Dickens", rating: 5, isbn: "9780140817997", notes: "🎁 from Papa"),
-            Book(title: "Don Quixote", author: "Miguel De Cervantes", rating: 4, isbn: "9788471890153", notes: ""),
-            Book(title: "Robinson Crusoe", author: "Daniel Defoe", rating: 5, isbn: "", notes: ""),
-            Book(title: "Gulliver's Travels", author: "Jonathan Swift", rating: 5, isbn: "", notes: ""),
-            Book(title: "Emma", author: "Jane Austen", rating: 5, isbn: "", notes: ""),
-            Book(title: "To Kill a Mockingbird", author: "Harper Lee", rating: 5, isbn: "", notes: ""),
-            Book(title: "Animal Farm", author: "George Orwell", rating: 4, isbn: "", notes: ""),
-            Book(title: "Gone with the Wind", author: "Margaret Mitchell", rating: 5, isbn: "", notes: ""),
-            Book(title: "The Fault in Our Stars", author: "John Green", rating: 5, isbn: "", notes: ""),
-            Book(title: "The Da Vinci Code", author: "Dan Brown", rating: 5, isbn: "", notes: ""),
-            Book(title: "Les Misérables ", author: "Victor Hugo", rating: 5, isbn: "", notes: ""),
-            Book(title: "Lord of the Flies ", author: "William Golding", rating: 5, isbn: "", notes: ""),
-            Book(title: "The Alchemist", author: "Paulo Coelho", rating: 5, isbn: "", notes: ""),
-            Book(title: "Life of Pi", author: "Yann Martel", rating: 5, isbn: "", notes: ""),
-            Book(title: "The Odyssey", author: "Homer", rating: 5, isbn: "", notes: ""),
-            //More books
-        ]
-        sort(books: &books)
-        return books
+        return []
     }
     func filter() {
         filteredBooks = books.filter { book in
