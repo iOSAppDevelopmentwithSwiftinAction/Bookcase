ca65fb608dd8bc1edfbb22df89cd98b6427021f8
Craig Grummitt <craiggrummitt@gmail.com>
2017-04-03 16:19:05 -0500

    Added bookCount / getBook

--- Bookcase/BooksManager.swift
+++ Bookcase/BooksManager.swift
@@ -9,64 +9,68 @@
 import Foundation
 
 enum SortOrder:Int {
-    case title
-    case author
+  case title
+  case author
 }
 
 class BooksManager {
-    lazy var books:[Book] = self.loadBooks()
-    var sortOrder:SortOrder = .title {
-        didSet {
-            sort(books:&books)
-        }
+  lazy var books:[Book] = self.loadBooks()
+  var bookCount:Int {return books.count}
+  func getBook(at index:Int)->Book {
+    return books[index]
+  }
+  var sortOrder:SortOrder = .title {
+    didSet {
+      sort(books:&books)
     }
-    func loadBooks()->[Book] {
-        return sampleBooks()
+  }
+  func loadBooks()->[Book] {
+    return sampleBooks()
+  }
+  func addBook(book:Book) {
+    books.append(book)
+    sort(books:&books)
+  }
+  func removeBook(at index:Int) {
+    books.remove(at: index)
+  }
+  func updateBook(at index:Int, with book:Book) {
+    books[index] = book
+    sort(books:&books)
+  }
+  func sampleBooks()->[Book] {
+    var books = [
+      Book(title: "Great Expectations", author: "Charles Dickens", rating: 5, isbn: "9780140817997", notes: "🎁 from Papa"),
+      Book(title: "Don Quixote", author: "Miguel De Cervantes", rating: 4, isbn: "9788471890153", notes: ""),
+      Book(title: "Robinson Crusoe", author: "Daniel Defoe", rating: 5, isbn: "", notes: ""),
+      Book(title: "Gulliver's Travels", author: "Jonathan Swift", rating: 5, isbn: "", notes: ""),
+      Book(title: "Emma", author: "Jane Austen", rating: 5, isbn: "", notes: ""),
+      Book(title: "To Kill a Mockingbird", author: "Harper Lee", rating: 5, isbn: "", notes: ""),
+      Book(title: "Animal Farm", author: "George Orwell", rating: 4, isbn: "", notes: ""),
+      Book(title: "Gone with the Wind", author: "Margaret Mitchell", rating: 5, isbn: "", notes: ""),
+      Book(title: "The Fault in Our Stars", author: "John Green", rating: 5, isbn: "", notes: ""),
+      Book(title: "The Da Vinci Code", author: "Dan Brown", rating: 5, isbn: "", notes: ""),
+      Book(title: "Les Misérables ", author: "Victor Hugo", rating: 5, isbn: "", notes: ""),
+      Book(title: "Lord of the Flies ", author: "William Golding", rating: 5, isbn: "", notes: ""),
+      Book(title: "The Alchemist", author: "Paulo Coelho", rating: 5, isbn: "", notes: ""),
+      Book(title: "Life of Pi", author: "Yann Martel", rating: 5, isbn: "", notes: ""),
+      Book(title: "The Odyssey", author: "Homer", rating: 5, isbn: "", notes: ""),
+      
+      //More books
+    ]
+    sort(books:&books)
+    return books
+  }
+  func sort(books:inout [Book]) {
+    switch sortOrder {
+    case .title:
+      books.sort(by: {
+        return ($0.title.localizedLowercase,$0.author.localizedLowercase) < ($1.title.localizedLowercase,$1.author.localizedLowercase)
+      })
+    case .author:
+      books.sort(by: {
+        return ($0.author.localizedLowercase,$0.title.localizedLowercase) < ($1.author.localizedLowercase,$1.title.localizedLowercase)
+      })
     }
-    func addBook(book:Book) {
-        books.append(book)
-        sort(books:&books)
-    }
-    func removeBook(at index:Int) {
-        books.remove(at: index)
-    }
-    func updateBook(at index:Int, with book:Book) {
-        books[index] = book
-        sort(books:&books)
-    }
-    func sampleBooks()->[Book] {
-        var books = [
-            Book(title: "Great Expectations", author: "Charles Dickens", rating: 5, isbn: "9780140817997", notes: "🎁 from Papa"),
-            Book(title: "Don Quixote", author: "Miguel De Cervantes", rating: 4, isbn: "9788471890153", notes: ""),
-            Book(title: "Robinson Crusoe", author: "Daniel Defoe", rating: 5, isbn: "", notes: ""),
-            Book(title: "Gulliver's Travels", author: "Jonathan Swift", rating: 5, isbn: "", notes: ""),
-            Book(title: "Emma", author: "Jane Austen", rating: 5, isbn: "", notes: ""),
-            Book(title: "To Kill a Mockingbird", author: "Harper Lee", rating: 5, isbn: "", notes: ""),
-            Book(title: "Animal Farm", author: "George Orwell", rating: 4, isbn: "", notes: ""),
-            Book(title: "Gone with the Wind", author: "Margaret Mitchell", rating: 5, isbn: "", notes: ""),
-            Book(title: "The Fault in Our Stars", author: "John Green", rating: 5, isbn: "", notes: ""),
-            Book(title: "The Da Vinci Code", author: "Dan Brown", rating: 5, isbn: "", notes: ""),
-            Book(title: "Les Misérables ", author: "Victor Hugo", rating: 5, isbn: "", notes: ""),
-            Book(title: "Lord of the Flies ", author: "William Golding", rating: 5, isbn: "", notes: ""),
-            Book(title: "The Alchemist", author: "Paulo Coelho", rating: 5, isbn: "", notes: ""),
-            Book(title: "Life of Pi", author: "Yann Martel", rating: 5, isbn: "", notes: ""),
-            Book(title: "The Odyssey", author: "Homer", rating: 5, isbn: "", notes: ""),
-            
-            //More books
-        ]
-        sort(books:&books)
-        return books
-    }
-    func sort(books:inout [Book]) {
-        switch sortOrder {
-        case .title:
-            books.sort(by: {
-                return ($0.title.localizedLowercase,$0.author.localizedLowercase) < ($1.title.localizedLowercase,$1.author.localizedLowercase)
-            })
-        case .author:
-            books.sort(by: {
-                return ($0.author.localizedLowercase,$0.title.localizedLowercase) < ($1.author.localizedLowercase,$1.title.localizedLowercase)
-            })
-        }
-    }
+  }
 }
--- Bookcase/BooksTableViewController.swift
+++ Bookcase/BooksTableViewController.swift
@@ -9,77 +9,77 @@
 import UIKit
 
 class BooksTableViewController: UITableViewController {
-    var booksManager:BooksManager = BooksManager()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+  var booksManager:BooksManager = BooksManager()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Uncomment the following line to preserve selection between presentations
+    // self.clearsSelectionOnViewWillAppear = false
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return booksManager.bookCount
+  }
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "bookCell", for: indexPath)
+    let book = booksManager.getBook(at: indexPath.row)
+    cell.textLabel?.text = book.title
+    cell.detailTextLabel?.text = book.author
+    cell.imageView?.image = book.cover ?? (UIImage(named: "book.jpg")!)
+    return cell
+  }
+  //override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+  //    return true
+  //}
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      booksManager.removeBook(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .fade)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  }
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if let selectedIndexPath = tableView.indexPathForSelectedRow,
+      let viewController = segue.destination as? BookViewController {
+      //Editing
+      viewController.book = booksManager.getBook(at: selectedIndexPath.row)
+      viewController.delegate = self
+    } else if let navController = segue.destination as? UINavigationController,
+      let viewController = navController.topViewController as? BookViewController {
+      //Adding
+      viewController.delegate = self
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return booksManager.books.count
-    }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "bookCell", for: indexPath)
-        let book = booksManager.books[indexPath.row]
-        cell.textLabel?.text = book.title
-        cell.detailTextLabel?.text = book.author
-        cell.imageView?.image = book.cover ?? (UIImage(named: "book.jpg")!)
-        return cell
-    }
-    //override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-    //    return true
-    //}
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            booksManager.removeBook(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-    }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let selectedIndexPath = tableView.indexPathForSelectedRow,
-            let viewController = segue.destination as? BookViewController {
-            //Editing
-            viewController.book = booksManager.books[selectedIndexPath.row]
-            viewController.delegate = self
-        } else if let navController = segue.destination as? UINavigationController,
-            let viewController = navController.topViewController as? BookViewController {
-            //Adding
-            viewController.delegate = self
-        }
-    }
-    @IBAction func changedSegment(_ sender: UISegmentedControl) {
-        guard let sortOrder = SortOrder(rawValue:sender.selectedSegmentIndex) else {return}
-        booksManager.sortOrder = sortOrder
-        tableView.reloadData()
-    }
-
+  }
+  @IBAction func changedSegment(_ sender: UISegmentedControl) {
+    guard let sortOrder = SortOrder(rawValue:sender.selectedSegmentIndex) else {return}
+    booksManager.sortOrder = sortOrder
+    tableView.reloadData()
+  }
+  
 }
 extension BooksTableViewController:BookViewControllerDelegate {
-    func saveBook(book:Book) {
-        if let selectedIndexPath = tableView.indexPathForSelectedRow {
-            //Update book
-            booksManager.updateBook(at: selectedIndexPath.row, with: book)
-            tableView.reloadRows(at: [selectedIndexPath], with: .none)
-        } else {
-            //Add book
-            booksManager.addBook(book: book)
-            let numRows = tableView.numberOfRows(inSection: 0)
-            let newIndexPath = IndexPath(row: numRows, section: 0)
-            tableView.insertRows(at: [newIndexPath], with: .bottom)
-        }
+  func saveBook(book:Book) {
+    if let selectedIndexPath = tableView.indexPathForSelectedRow {
+      //Update book
+      booksManager.updateBook(at: selectedIndexPath.row, with: book)
+      tableView.reloadRows(at: [selectedIndexPath], with: .none)
+    } else {
+      //Add book
+      booksManager.addBook(book: book)
+      let numRows = tableView.numberOfRows(inSection: 0)
+      let newIndexPath = IndexPath(row: numRows, section: 0)
+      tableView.insertRows(at: [newIndexPath], with: .bottom)
     }
+  }
 }
