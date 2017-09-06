5afa79b2a0385679be588263dd5293ad00580b00
Craig Grummitt <craiggrummitt@gmail.com>
2017-09-06 22:35:19 +0100

    Updated cell.imageView

--- Bookcase/Book.swift
+++ Bookcase/Book.swift
@@ -8,19 +8,28 @@
 
 import UIKit
 struct Book {
-    var title:String
-    var author:String
-    var rating:Double
-    var isbn:String
-    var notes:String
-    var cover:UIImage?
-    
-    init(title:String,author:String,rating:Double,isbn:String,notes:String,cover:UIImage? = nil) {
-        self.title = title
-        self.author = author
-        self.rating = rating
-        self.isbn = isbn
-        self.notes = notes
-        self.cover = cover
+  static let defaultCover = UIImage(named: "book.jpg")!
+  var title:String
+  var author:String
+  var rating:Double
+  var isbn:String
+  var notes:String
+  var cover:UIImage {
+    get {
+      return image ?? Book.defaultCover
     }
+    set {
+      image = newValue
+    }
+  }
+  private var image:UIImage? = nil
+  
+  init(title:String,author:String,rating:Double,isbn:String,notes:String,cover:UIImage? = nil) {
+    self.title = title
+    self.author = author
+    self.rating = rating
+    self.isbn = isbn
+    self.notes = notes
+    self.image = cover
+  }
 }
