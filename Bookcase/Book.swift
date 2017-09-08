139649c9278957257a3f105fc320ede4a911a7d5
Craig Grummitt <craiggrummitt@gmail.com>
2017-09-08 15:02:05 +0100

    Update for Swift 4 / Codable

--- Bookcase/Book.swift
+++ Bookcase/Book.swift
@@ -7,15 +7,8 @@
 //
 
 import UIKit
-internal struct Key {
-    static let title = "title"
-    static let author = "author"
-    static let rating = "rating"
-    static let isbn = "isbn"
-    static let notes = "notes"
-}
 
-class Book: NSObject, NSCoding {
+struct Book: Codable {
     static let defaultCover = UIImage(named: "book.jpg")!
     var title:String
     var author:String
@@ -30,7 +23,7 @@ class Book: NSObject, NSCoding {
     var hasCoverImage:Bool {
         return image != nil
     }
-    private var image:UIImage?
+    private var image:UIImage? = nil
     
     init(title:String,author:String,rating:Double,isbn:String,notes:String,cover:UIImage? = nil) {
         self.title = title
@@ -40,31 +33,14 @@ class Book: NSObject, NSCoding {
         self.notes = notes
         self.image = cover
     }
-    // MARK: NSCoding
-    convenience required init?(coder aDecoder: NSCoder) {
-        let rating = aDecoder.decodeDouble(forKey: Key.rating)
-        guard let title = aDecoder.decodeObject(forKey: Key.title) as? String,
-            let author = aDecoder.decodeObject(forKey:Key.author) as? String,
-            let isbn = aDecoder.decodeObject(forKey:Key.isbn) as? String,
-            let notes = aDecoder.decodeObject(forKey:Key.notes) as? String
-            else { return nil }
-        
-        self.init(
-            title: title,
-            author: author,
-            rating: rating,
-            isbn: isbn,
-            notes: notes
-        )
+    // MARK: Codable
+    enum CodingKeys: String, CodingKey {
+      case title
+      case author
+      case rating
+      case isbn
+      case notes
     }
-    func encode(with aCoder: NSCoder) {
-        aCoder.encode(self.title, forKey: Key.title)
-        aCoder.encode(self.author, forKey: Key.author)
-        aCoder.encode(self.rating, forKey: Key.rating)
-        aCoder.encode(self.isbn, forKey: Key.isbn)
-        aCoder.encode(self.notes, forKey: Key.notes)
-    }
-
 }
 //extension Book:Equatable {}
 func ==(lhs: Book, rhs: Book) -> Bool {
