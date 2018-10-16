d1f5c2f61617d86df032bceb8e45d8a05c585797
Craig Grummitt <craiggrummitt@gmail.com>
2018-10-17 00:07:11 +0100

    Update for Swift 4.2

--- Bookcase/BooksTableViewController.swift
+++ Bookcase/BooksTableViewController.swift
@@ -48,7 +48,7 @@ class BooksTableViewController: UITableViewController {
   //override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
   //    return true
   //}
-  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
     if editingStyle == .delete {
       booksManager.removeBook(at: indexPath.row)
       tableView.deleteRows(at: [indexPath], with: .fade)
